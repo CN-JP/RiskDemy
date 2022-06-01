44e2b36bc1aad0a46e0861238edcb8e424c427f5
Nonso Johnpaul Akamnonu <osnon.nonso@gmail.com>
2022-06-01 15:47:56 +0100

    riskdemy v.1 homepage

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+// autoType Title-3
+// let typed = new Typed(".auto-type", {
+//   strings: ["JavaScript", "React", "HTML", "CSS and Sass", "Bootstrap", "SQL", "CLI"],
+//   typeSpeed: 150,
+//   backSpeed: 150,
+//   loop: true
+// })
